b0b726d7244ddc29d48ddf395a36e18aa846c5d2
Willian Krause <krause.willian@gmail.com>
2017-08-18 09:44:19 -0300

    PrivateRoute working

--- src/PrivateRoute.js
+++ src/PrivateRoute.js
@@ -1,17 +1,20 @@
-import React, {Component} from 'react';
+import React, {Component} from "react";
 import {Route, Redirect} from "react-router-dom";
 
-const PrivateRoute = ({ component: Component, ...rest }) => (
-    <Route {...rest} render={props => (
-        localStorage.getItem('token') ? (
-                <Component {...props}/>
-            ) : (
-                <Redirect to={{
-                    pathname: '/login',
-                    state: { from: props.location }
-                }}/>
-            )
-    )}/>
-);
+const PrivateRoute = ({component: Component, ...rest}) => {
+    console.log(rest);
+    return (
+        <Route {...rest} render={props => (
+            localStorage.getItem('token') ? (
+                    <Component {...props}/>
+                ) : (
+                    <Redirect to={{
+                        pathname: '/',
+                        state: {from: props.location}
+                    }}/>
+                )
+        )}/>
+    )
+};
 
 export default PrivateRoute;
--- src/index.js
+++ src/index.js
@@ -1,20 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import Login from './components/Login';
-import PrivateRoute from './PrivateRoute';
-import registerServiceWorker from './registerServiceWorker';
-import './css/reset.css'
-import './css/timeline.css'
-import './css/login.css'
-import {BrowserRouter as Router, Route, Switch, Redirect} from "react-router-dom";
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import Login from "./components/Login";
+import PrivateRoute from "./PrivateRoute";
+import registerServiceWorker from "./registerServiceWorker";
+import "./css/reset.css";
+import "./css/timeline.css";
+import "./css/login.css";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 
 ReactDOM.render(
     <Router>
         <Switch>
             <Route path="/" exact component={Login}/>
-            <Route path="/timeline" component={App} />
-            <Redirect path="*" to="/" />
+            <PrivateRoute path="/timeline" component={App} />
         </Switch>
     </Router>
     , document.getElementById('root'));
