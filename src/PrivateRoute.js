7da86a9f3f9aac4c45127358fb5565702a96cf80
Willian Krause <krause.willian@gmail.com>
2017-08-18 10:54:55 -0300

    Allowing public timeline

--- src/PrivateRoute.js
+++ src/PrivateRoute.js
@@ -1,17 +1,23 @@
 import React, {Component} from "react";
-import {Route, Redirect} from "react-router-dom";
+import {Route, Redirect, matchPath} from "react-router-dom";
 
 const PrivateRoute = ({component: Component, ...rest}) => {
     console.log(rest);
+    const match = matchPath(rest.location.pathname, {
+        path: '/timeline',
+        exact: true,
+        strict: false
+    });
+    console.log(match);
     return (
         <Route {...rest} render={props => (
-            localStorage.getItem('token') ? (
-                    <Component {...props}/>
-                ) : (
+            match && !localStorage.getItem('token') ? (
                     <Redirect to={{
                         pathname: '/',
                         state: {from: props.location}
                     }}/>
+                ) : (
+                    <Component {...props}/>
                 )
         )}/>
     )
